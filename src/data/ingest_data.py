c0db04e8db61b0b3801c0fb92dfe3e9bd1bbb808
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-27 16:20:00 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -13,10 +13,27 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    raise NotImplementedError("Implementar esta función")
 
+def ingest_data():
+    """Ingeste los datos externos a la capa landing del data lake.
+
+    Del repositorio jdvelasq/datalabs/precio_bolsa_nacional/xls/ descarge los
+    archivos de precios de bolsa nacional en formato xls a la capa landing. La
+    descarga debe realizarse usando únicamente funciones de Python.
+
+    """
+import pandas as pd
+import wget
+import os
+
+link = "https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls"
+os.chdir('data_lake/landing')
+for yearnum in range (1995,2021):
+    wdir = link + "/" + format(yearnum) + ".xlsx"
+    pd.read_excel(wget.download(wdir))
+os.chdir('../../')
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
+    ingest_data()
