00b9a2f3331f13ba10f58988bad762d679194acf
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-29 17:38:09 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -9,8 +9,32 @@ def transform_data():
     """
     raise NotImplementedError("Implementar esta función")
 
+from asyncore import read
+from email import header
+import pandas as pd
+
+for yearnum in range (1995,2022):
+    if yearnum == 2016 or yearnum == 2017:
+        read_file = pd.read_excel('data_lake/landing/{}.xls'.format(yearnum),header = None)
+        # Mantenga solo las filas con al menos 20 valores que no sean NA
+        df = read_file.dropna(axis =0, thresh= 20)
+        # Omitir la primera fila
+        df = df.iloc[1:]
+        # Establecer la primer columna como formato fecha
+        df[0] = pd.to_datetime(df[0], format="%Y/%m/%d")
+        df.to_csv('data_lake/raw/{}.csv'.format(yearnum), index= False, header = True)
+    else:
+        read_file = pd.read_excel('data_lake/landing/{}.xlsx'.format(yearnum),header = None)
+        # Mantenga solo las filas con al menos 20 valores que no sean NA
+        df = read_file.dropna(axis =0, thresh= 20)
+        # Omitir la primera fila
+        df = df.iloc[1:]
+        # Establecer la primer columna como formato fecha
+        df[0] = pd.to_datetime(df[0], format="%Y/%m/%d")
+        df.to_csv('data_lake/raw/{}.csv'.format(yearnum), index= False, header = True)     
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    transform_data()
