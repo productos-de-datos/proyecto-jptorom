8b9a2526f7b4b06853b9f2cc905449fb79dee1e7
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-29 21:03:08 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -36,7 +36,9 @@ for yearnum in range (1995,2022):
         df = df.iloc[1:,:25]
         df.columns = ["fecha", "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", 
         "16", "17", "18", "19", "20", "21", "22", "23"] 
-        df.to_csv('data_lake/raw/{}.csv'.format(yearnum), index_label= False, index = False)     
+        df.to_csv('data_lake/raw/{}.csv'.format(yearnum), index_label= False, index = False)
+        
+    #raise NotImplementedError("Implementar esta función")
 
 if __name__ == "__main__":
     import doctest
