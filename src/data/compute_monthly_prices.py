16fcbb6c31d5cab272658b4bba9b682aa3c13b91
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-30 15:20:28 -0500

    Update compute_monthly_prices.py

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -12,10 +12,27 @@ def compute_monthly_prices():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    #raise NotImplementedError("Implementar esta función")
+
+    import pandas as pd
+
+    # Se carga la data
+    readfile = pd.read_csv('data_lake/cleansed/precios-horarios.csv')
+    # Se toman únicamente las columnas Fecha y precio
+    readfile = readfile[["Fecha","precio"]]
+    # Se convierte el formato del campo Fecha
+    readfile["Fecha"] = pd.to_datetime(readfile["Fecha"], format="%Y/%m/%d")
+    # Se cambia el día de cada fecha, colocando el primer día de cada mes
+    readfile["Fecha"] = readfile["Fecha"].dt.to_period("M").dt.to_timestamp()
+    # Se agrupa por fecha y se saca la media del precio
+    df = readfile.groupby(["Fecha"]).mean().reset_index()
+    # Se convierte el dataframe a formato csv y se guarda en la carpeta businees
+    df.to_csv('data_lake/business/precios-mensuales.csv',index=False)
+    return df
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    compute_monthly_prices()
