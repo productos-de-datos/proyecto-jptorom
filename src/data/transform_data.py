0591b04bad07b9bcf206c03ba1662791f30e942e
jptorom <101909873+jptorom@users.noreply.github.com>
2022-06-29 21:30:51 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -17,17 +17,19 @@ for yearnum in range (1995,2022):
         df = read_file.dropna(how = "any")
         # Se eliminan columnas sobrantes y se omite la primera fila
         df = df.iloc[1:,:25]
-        df.columns = ["fecha", "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", 
-        "16", "17", "18", "19", "20", "21", "22", "23"] 
-        df["fecha"] = pd.to_datetime(df["fecha"], format="%Y/%m/%d")
+        df.columns = ["Fecha","H00","H01","H02","H03",
+        "H04","H05","H06","H07","H08","H09","H10","H11","H12","H13",
+        "H14","H15","H16","H17","H18","H19","H20","H21","H22","H23"]
+        df["Fecha"] = pd.to_datetime(df["Fecha"], format="%Y/%m/%d")
         df.to_csv('data_lake/raw/{}.csv'.format(yearnum), index_label= False, index = False)  
     if yearnum in range (2018,2022):
         read_file = pd.read_excel('data_lake/landing/{}.xlsx'.format(yearnum),index_col=None, header = None)
         # Se eliminan columnas sobrantes y se omite la primera fila
         df = read_file.iloc[1:,:25]
-        df.columns = ["fecha", "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", 
-        "16", "17", "18", "19", "20", "21", "22", "23"]
-        df["fecha"] = pd.to_datetime(df["fecha"], format="%Y/%m/%d")
+        df.columns = ["Fecha","H00","H01","H02","H03",
+        "H04","H05","H06","H07","H08","H09","H10","H11","H12","H13",
+        "H14","H15","H16","H17","H18","H19","H20","H21","H22","H23"]
+        df["Fecha"] = pd.to_datetime(df["Fecha"], format="%Y/%m/%d")
         df.to_csv('data_lake/raw/{}.csv'.format(yearnum), index_label= False, index = False)  
 
     if yearnum in range (1995,2016):
@@ -36,9 +38,10 @@ for yearnum in range (1995,2022):
         df = read_file.dropna(how = "any")
         # Se eliminan columnas sobrantes y se omite la primera fila
         df = df.iloc[1:,:25]
-        df.columns = ["fecha", "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", 
-        "16", "17", "18", "19", "20", "21", "22", "23"]
-        df["fecha"] = pd.to_datetime(df["fecha"], format="%Y/%m/%d")
+        df.columns = ["Fecha","H00","H01","H02","H03",
+        "H04","H05","H06","H07","H08","H09","H10","H11","H12","H13",
+        "H14","H15","H16","H17","H18","H19","H20","H21","H22","H23"]
+        df["Fecha"] = pd.to_datetime(df["Fecha"], format="%Y/%m/%d")
         df.to_csv('data_lake/raw/{}.csv'.format(yearnum), index_label= False, index = False)     
 
 if __name__ == "__main__":
@@ -46,4 +49,3 @@ if __name__ == "__main__":
 
     doctest.testmod()
     transform_data()
-    
