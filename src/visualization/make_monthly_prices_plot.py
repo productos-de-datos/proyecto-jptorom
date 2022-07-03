694826dcd0ff57242f195c45d98c6cc1ca791303
jptorom <101909873+jptorom@users.noreply.github.com>
2022-07-03 15:41:32 -0500

    Update and rename make make_monthly_prices_plot.py to make_monthly_prices_plot.py

---
+++ src/visualization/make_monthly_prices_plot.py
@@ -0,0 +1,47 @@
+
+from pandas import to_datetime
+import os
+
+
+def make_monthly_prices_plot():
+    """Crea un grafico de lines que representa los precios promedios diarios.
+
+    Usando el archivo data_lake/business/precios-diarios.csv, crea un grafico de
+    lines que representa los precios promedios diarios.
+
+    El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
+
+    """
+    #raise NotImplementedError("Implementar esta función")
+
+
+if __name__ == "__main__":
+    import doctest
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    # Se obtienen los datos
+    datos = pd.read_csv("data_lake/business/precios-mensuales.csv", header= 0)
+    #Se ajusta el formato del campo Fecha
+    datos['Fecha'] = pd.to_datetime(datos['Fecha'], format='%Y-%m-%d')
+    # Se asignan a valores a los ejes
+    x_values= datos["Fecha"]
+    y_values = datos["precio"]
+
+    plt.figure(figsize=(14, 4)) 
+    plt.plot(x_values,y_values,"g")
+    plt.ylabel("Precio")
+    plt.xlabel("Fecha")
+    plt.title("Precio Promedio Mensual de Energía")
+    plt.savefig("data_lake/business/reports/figures/monthly_prices.png")
+
+def test_09():
+    assert os.path.isfile('data_lake/business/reports/figures/daily_prices.png') is True
+
+    #doctest.testmod()
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    make_monthly_prices_plot()
