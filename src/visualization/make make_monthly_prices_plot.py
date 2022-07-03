b31639b4254e1cfb59e1d17c61326dfbf02d2003
jptorom <101909873+jptorom@users.noreply.github.com>
2022-07-03 15:40:49 -0500

    Update make make_monthly_prices_plot.py

--- src/visualization/make make_monthly_prices_plot.py
+++ src/visualization/make make_monthly_prices_plot.py
@@ -1,46 +1,32 @@
-
-from pandas import to_datetime
-
-
-def make_monthly_prices_plot():
-    """Crea un grafico de lines que representa los precios promedios diarios.
-
-    Usando el archivo data_lake/business/precios-diarios.csv, crea un grafico de
-    lines que representa los precios promedios diarios.
-
-    El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
-
-    """
-    #raise NotImplementedError("Implementar esta función")
-
-
-if __name__ == "__main__":
-    import doctest
+def make_monthly_prices_plot(): 
+    """Crea un grafico de lines que representa los precios promedios mensuales. 
+ 
+    Usando el archivo data_lake/business/precios-mesuales.csv, crea un grafico de 
+    lines que representa los precios promedios mensuales. 
+ 
+    El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png. 
+ 
+    """ 
     import pandas as pd
     import matplotlib.pyplot as plt
-    import numpy as np
 
-    # Se obtienen los datos
-    datos = pd.read_csv("data_lake/business/precios-mensuales.csv", header= 0)
-    #Se ajusta el formato del campo Fecha
-    datos['Fecha'] = pd.to_datetime(datos['Fecha'], format='%Y-%m-%d')
-    # Se asignan a valores a los ejes
-    x_values= datos["Fecha"]
-    y_values = datos["precio"]
+    precios_mensuales = pd.read_csv('data_lake/business/precios-mensuales.csv')
+    precios_mensuales['Fecha'] = pd.to_datetime(precios_mensuales["Fecha"])
 
-    plt.figure(figsize=(14, 4)) 
-    plt.plot(x_values,y_values)
-    plt.ylabel("Precio")
-    plt.xlabel("Fecha")
-    plt.title("Precio Promedio Mensual de Energía")
-    plt.savefig("data_lake/business/reports/figures/monthly_prices.png")
+    x = precios_mensuales['Fecha']
+    y = precios_mensuales['precio']
 
-
-    #doctest.testmod()
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+    plt.figure(figsize=(10, 4)) 
+    plt.plot(x, y, label='Promedio Mensual') 
+    plt.title('Precio Promedio Mensual') 
+    plt.xlabel('Fecha') 
+    plt.ylabel('Precio') 
+    plt.savefig("data_lake/business/reports/figures/monthly_prices.png") 
+    
+ 
+if __name__ == "__main__": 
+    import doctest 
+    
+    doctest.testmod() 
     make_monthly_prices_plot()
-
    
