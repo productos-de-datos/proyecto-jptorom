c28467f675f6f19e452ef581088f1233402a4efe
jptorom <101909873+jptorom@users.noreply.github.com>
2022-07-03 13:37:27 -0500

    Update make_daily_prices_plot.py

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -1,3 +1,7 @@
+
+from pandas import to_datetime
+
+
 def make_daily_prices_plot():
     """Crea un grafico de lines que representa los precios promedios diarios.
 
@@ -7,10 +11,32 @@ def make_daily_prices_plot():
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    # Se obtienen los datos
+    datos = pd.read_csv("data_lake/business/precios-diarios.csv", header= 0)
+    #Se ajusta el formato del campo Fecha
+    datos["Fecha"] = pd.to_datetime(datos["Fecha"])
+    # Se asignan a valores a los ejes
+    x_values= datos["Fecha"]
+    y_values = datos["precio"]
+
+    grafico = plt.plot(x_values,y_values,"g")
+    plt.ylabel("Precio")
+    plt.xlabel("Fecha")
+    plt.title("Precio Promedio Diario de Energía")
+    plt.savefig("data_lake/business/reports/figures/daily_prices.png")
+
+    #doctest.testmod()
+if __name__ == "__main__":
+    import doctest
 
     doctest.testmod()
+    make_daily_prices_plot()
