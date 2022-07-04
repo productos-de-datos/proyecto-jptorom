d0e0ffc37993de4d8f89488b0662aa41f44a6698
jptorom <101909873+jptorom@users.noreply.github.com>
2022-07-03 22:52:50 -0500

    Update make_features.py

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -1,25 +1,24 @@
-'''
-    Enunciado:
-    Cree el archivo data_lake/business/features/precios-diarios.csv. Este
-    archivo contiene la información para pronosticar los precios diarios de la
-    electricidad con base en los precios de los días pasados. Las columnas
-    correspoden a las variables explicativas del modelo, y debe incluir,
-    adicionalmente, la fecha del precio que se desea pronosticar y el precio
-    que se desea pronosticar (variable dependiente).
+"""
+Enunciado:
+Cree el archivo data_lake/business/features/precios-diarios.csv. Este
+archivo contiene la información para pronosticar los precios diarios de la
+electricidad con base en los precios de los días pasados. Las columnas
+correspoden a las variables explicativas del modelo, y debe incluir,
+adicionalmente, la fecha del precio que se desea pronosticar y el precio
+que se desea pronosticar (variable dependiente).
 
-    En la carpeta notebooks/ cree los notebooks de jupyter necesarios para
-    analizar y determinar las variables explicativas del modelo.
+En la carpeta notebooks/ cree los notebooks de jupyter necesarios para
+analizar y determinar las variables explicativas del modelo.
     
-Documentación make_features(): Dado que en nuestro dataframe únicamente tenemos dos columnas, de las cuales una es dependiente, procedemos a guardar los datos
-sin mayor procesamiento.
+Documentación make_features(): Dado que en nuestro dataframe únicamente
+tenemos dos columnas, de las cuales una es dependiente, procedemos a 
+guardar los datos sin mayor procesamiento.
 
-'''
-
+"""
 import pandas as pd
 def make_features():
     datos = pd.read_csv('data_lake/business/precios-diarios.csv', index_col=0)
-    datos.to_csv('data_lake/business/features/precios-diarios.csv', index=True)
-    
+    datos.to_csv('data_lake/business/features/precios-diarios.csv', index=True)    
     #raise NotImplementedError("Implementar esta función")
 
 if __name__ == "__main__":
