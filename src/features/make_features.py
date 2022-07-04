80281073a6ea611c3f1793f16f6da147a9df7327
jptorom <101909873+jptorom@users.noreply.github.com>
2022-07-03 22:04:18 -0500

    Update make_features.py

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -1,17 +1,3 @@
-"""
-
-Requerimiento:
-Cree el archivo data_lake/business/features/precios-diarios.csv. Este
-archivo contiene la información para pronosticar los precios diarios de la
-electricidad con base en los precios de los días pasados. Las columnas
-correspoden a las variables explicativas del modelo, y debe incluir,
-adicionalmente, la fecha del precio que se desea pronosticar y el precio
-que se desea pronosticar (variable dependiente).
-
-En la carpeta notebooks/ cree los notebooks de jupyter necesarios para
-analizar y determinar las variables explicativas del modelo.
-
-"""
 import os
 def make_features():
     """Prepara datos para pronóstico.
